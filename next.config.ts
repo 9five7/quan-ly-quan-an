8db2a6c651b160a342d32dc3af08de7ef697bfcc
9five7 <trucpro1282003@gmail.com>
2025-04-03 15:15:13 +0700

    SS

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['ik.imagekit.io','localhost','via.placeholder.com'], // Thêm domain của ImageKit
+    domains: ['ik.imagekit.io','backendquanlyquanan.onrender.com','via.placeholder.com'], // Thêm domain của ImageKit
   },
 
 };
